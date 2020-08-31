8f0ad24771e765a030be12ceefa871e92daf47d2
Hannah Angle <hannahcangle@gmail.com>
2020-08-31 12:37:33 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,7 +2,6 @@ class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
-    # create a new Student object given a row from the database
     new_student = self.new 
     new_student.id = row[0]
     new_student.name = row[1]
@@ -11,8 +10,17 @@ class Student
   end
 
   def self.all
-    # retrieve all the rows from the "Students" database
-    # remember each row should be a new instance of the Student class
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 9;
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
+    
   end
 
   def self.find_by_name(name)
