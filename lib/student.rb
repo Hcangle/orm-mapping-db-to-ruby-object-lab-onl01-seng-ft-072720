299417ecdf9ea75a4b9acae6c813db38069c771d
Hannah Angle <hannahcangle@gmail.com>
2020-08-31 12:40:00 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -60,4 +60,27 @@ class Student
     sql = "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
   end
+  
+  def self.all_students_in_grade_9
+    sql = <<-SQL
+      SELECT COUNT(*)
+      FROM students
+      WHERE grade = 9;
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
+  
+  def self.all_students_below_12th_grade
+    sql = <<-SQL
+    SELECT COUNT(*)
+    FROM students
+    WHERE grade < 12
+  SQL
+  DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
 end
