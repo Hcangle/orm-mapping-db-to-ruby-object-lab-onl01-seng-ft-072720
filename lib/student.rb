bed33fa4bd99b71a2fbd35a97666e9fd25fe0148
Hannah Angle <hannahcangle@gmail.com>
2020-08-31 12:46:42 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -108,4 +108,16 @@ def self.first_student_in_grade_10
       self.new_from_db(row)
     end.first
   end
+def self.all_students_in_grade_X(grade)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = ?
+      ORDER BY students.id
+    SQL
 
+    DB[:conn].execute(sql, grade).map do |row|
+      self.new_from_db(row)
+    end
+  end
+end
